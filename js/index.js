fce6a497ab636e49a88856c4a8c577861f7134a0
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2020-03-14 21:58:12 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var widget = "https://discordapp.com/api/guilds/367648314184826880/widget.json";
 
 var openInvite = function (data) {
+    $(".text").text(JSON.parse(data)["instant_invite"]);
     window.location = JSON.parse(data)["instant_invite"];
 };
 

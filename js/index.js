c2f2d33ee6bbfb5497a4d49f320da1f2132bcb2b
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2020-05-21 19:40:37 +0200

    unnötiges Zeugs entfernt

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
-var widget = "https://discord.com/api/guilds/367648314184826880/widget.json";
+const widget = "https://discord.com/api/guilds/367648314184826880/widget.json";
+const text = document.getElementById("text");
+text.textContent = "";
 
-var openInvite = function (data) {
-    $(".text").text(JSON.parse(data)["instant_invite"]);
-    window.location = JSON.parse(data)["instant_invite"];
+const openInvite = function (data) {
+    const invite = JSON.parse(data)["instant_invite"];
+    const item = document.createElement("a");
+    item.textContent = invite;
+    item.href = invite;
+    text.appendChild(item);
+
+    window.location = invite;
 };
 
-$.get(widget, openInvite, 'text');
+fetch(widget)
+    .then(function(response) {
+            response.text().then(openInvite);
+        }, function (error) {
+            console.log(error);
+        }
+    );

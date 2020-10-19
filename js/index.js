49a292b7c5493474f5d5fc408ae8b2c6a64729a8
Joshix <joshix.dev@gmail.com>
2020-10-19 19:55:59 +0200

    improve site

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 const widget = "https://discord.com/api/guilds/367648314184826880/widget.json";
-const text = document.getElementById("text");
+const link = document.getElementById("link");
 
 const openInvite = function (data) {
     const invite = JSON.parse(data)["instant_invite"];
-    const item = document.createElement("a");
-    item.textContent = invite;
-    item.href = invite;
-    text.appendChild(item);
-
-    window.location = invite;
+    link.href = invite;
+    //window.location = invite;
 };
 
 fetch(widget)
